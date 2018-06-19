d223153f84ae9138477a9c284e9d12f68ab3c097
Idalia Rojas <klauz7biomed@gmail.com>
2018-06-19 15:09:42 -0500

    Add files via upload
    This script was used to merge the gvcf files after run the haplotype caller tool. This scrip was  written by Greg and the original version is available at https://github.com/owensgl/argentina_helianthus/blob/master/Genotype_gvcf.sh

---
+++ SNP_Calling/bin/Merge_gvcf_files.sh
@@ -0,0 +1,26 @@
+#After run Haplotype Caller this you must merge the gvcf files to get a final VCF file with all the samples and SNPs. 
+#- Save all the gvcf in the same directory
+#- I used a modified version of the Greg Owens script available at:
+# https://github.com/owensgl/argentina_helianthus/blob/master/Genotype_gvcf.sh
+
+
+combinedGVCFs='/home/rojas/Zmays/gvcf'
+ls $combinedGVCFs | grep "vcf" | grep -v ".idx"   > GVCFs.samplelist.txt
+tmp=""
+while read prefix
+do
+        tmp="$tmp --variant $combinedGVCFs/$prefix"
+done < GVCFs.samplelist.txt
+
+java -Xmx40g -jar /home/rojas/bin/GenomeAnalysisTK-3.8-0-ge9d806836/GenomeAnalysisTK.jar \
+        -nt 15 \
+        -l INFO \
+        -R /home/rojas/Zmays/Zea_mays.AGPv4.dna.toplevel.fa \
+        -log /home/rojas/Zmays/log/GenotypeGVCFsAGPv4.log \
+        -T GenotypeGVCFs \
+        $tmp \
+        -o /home/rojas/Zmays/Zea_mays.AGPv4.vcf \
+	-hets 0.01 \
+        --max_alternate_alleles 4
+
+#The output is ready to be filter
